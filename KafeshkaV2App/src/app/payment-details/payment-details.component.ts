feef07ffd11859ba221090aeffd18cc86c31e012
Alex Redko <a.redko@mail.com>
2024-03-19 08:09:20 +0000

    add auto population fields

--- KafeshkaV2App/src/app/payment-details/payment-details.component.ts
+++ KafeshkaV2App/src/app/payment-details/payment-details.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {PaymentDetailsFormComponent} from "./payment-details-form/payment-details-form.component";
 import {PaymentDetailService} from "../shared/payment-detail.service";
 import {NgForOf} from "@angular/common";
+import {PaymentDetail} from "../shared/payment-detail.model";
 
 @Component({
   selector: 'app-payment-details',
@@ -13,11 +14,15 @@ import {NgForOf} from "@angular/common";
   templateUrl: './payment-details.component.html',
   styles: ``
 })
-export class PaymentDetailsComponent implements OnInit{
-constructor(public service:PaymentDetailService) {
-}
+export class PaymentDetailsComponent implements OnInit {
+  constructor(public service: PaymentDetailService) {
+  }
 
   ngOnInit(): void {
-         this.service.refreshList();
-    }
+    this.service.refreshList();
+  }
+
+  populateForm(pd: PaymentDetail) {
+    this.service.formData = Object.assign({}, pd);
+  }
 }
