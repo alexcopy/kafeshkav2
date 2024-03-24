740730692d0086731c0a9204e3de93c96652e811
Alex Redko <a.redko@mail.com>
2024-03-24 23:06:33 +0000

    Fix Login, Logout  and Register

--- KafeshkaV2/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ KafeshkaV2/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -88,6 +88,10 @@ namespace KafeshkaV2.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("/");
+            }
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
--- KafeshkaV2/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ KafeshkaV2/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -14,10 +14,10 @@ namespace KafeshkaV2.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<KafeshkaAppUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<KafeshkaAppUser> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
