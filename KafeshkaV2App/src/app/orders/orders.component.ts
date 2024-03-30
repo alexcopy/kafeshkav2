be1eafc10a378f22ec76adbe4782be1c98ca757f
Alex Redko <a.redko@mail.com>
2024-03-30 14:36:21 +0000

    add Gtotal calc

--- KafeshkaV2App/src/app/orders/orders.component.ts
+++ KafeshkaV2App/src/app/orders/orders.component.ts
@@ -43,13 +43,23 @@ export class OrdersComponent implements OnInit {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
     dialogConfig.disableClose = true;
-    dialogConfig.minWidth="50%";
+    dialogConfig.minWidth = "50%";
     dialogConfig.data = {orderItemIndex, OrderId}
-    this.dialog.open(OrderItemsComponent, dialogConfig);
+    this.dialog.open(OrderItemsComponent, dialogConfig).afterClosed().subscribe(res => {
+      this.updateGrandTotal();
+    });
   }
 
   onDeleteOrderItem(OrderItemId: number, i: number) {
     this.service.orderItems.splice(i, 1);
+    this.updateGrandTotal();
+  }
+
+  updateGrandTotal() {
+    this.service.formData.GTotal = this.service.orderItems.reduce((prev, curr) => {
+      return prev + curr.Total;
+    }, 0);
+    this.service.formData.GTotal = parseFloat((this.service.formData.GTotal).toFixed(2));
   }
 }
 
